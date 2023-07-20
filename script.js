e415634414f7b6e65d67327ecf7f8bff165ca137
JesseAger <agerjesse6@gmail.com>
2023-07-20 19:42:13 +0300

    script

---
+++ script.js
@@ -0,0 +1,26 @@
+// for loop
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// while loop
+var count = 0;
+while (count < 5) {
+  console.log(count);
+  count++;
+}
+
+// do-while loop
+var num = 0;
+do {
+  console.log(num);
+  num++;
+} while (num < 5);
+
+
+#######################
+function greet(name) {
+  console.log('Hello, ' + name + '!');
+}
+
+greet('Alice'); // Output: Hello, Alice!
